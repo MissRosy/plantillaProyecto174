4095ca40ca53048e6e15193297144236f43e0425
MissRosy <88013528+MissRosy@users.noreply.github.com>
2023-05-16 14:38:13 -0600

    Add files via upload

---
+++ js/createModel.js
@@ -0,0 +1,21 @@
+AFRAME.registerComponent("createmodels", {
+  init: async function() {
+    //VARIABLE PARA GUARDAR EL RESULTADO DE MANDAR LLAMAR A LA FUNCIÓN getModels()
+    
+    //VARIABLE PARA GUARDAR LO ANTERIOR COMO MATRIZ
+   
+    //HACER MAPEO A LA MATRIZ ANTERIOR Y MANDAR LLAMAR A LA FUNCIÓN createModel()
+    
+  },
+
+  //FUNCIÓN PARA OBTENER LOS DATOS DEL ARCHIVO JSON
+  getModels: function() {
+   
+    
+  },
+  
+  //FUNCIÓN PARA CREAR EL MODELO
+  createModel: function(model) {
+
+  }
+});
